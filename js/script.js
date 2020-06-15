93156a8ecd2fdfe4faa0295fc170f3d086a170ee
PauloAlves8039 <pj38alves@gmail.com>
2020-06-15 20:33:27 -0300

    Definindo a movimentação do personagem

--- js/script.js
+++ js/script.js
@@ -15,6 +15,8 @@ snake[0] = {
     y: 8 * box
 };
 
+let direction = 'right';
+
 /**
  * Define a criação do background.
  * @function
@@ -37,5 +39,55 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+document.addEventListener('keydown', update);
+
+/**
+ * Atualiza a movimentação do personagem de acordo com a tecla pessionada.
+ * @function
+ * @name update
+ * 
+ * @param {*} event parâmetro para chamada de evento.
+ */
+function update(event) {
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
+/**
+ * Responsável pela inicialização do jogo.
+ * @function
+ * @nane iniciarJogo
+ */
+function iniciarJogo() {
+    
+    if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == 'right') snakeX += box;
+    if(direction == 'left') snakeX -= box;
+    if(direction == 'up') snakeY -= box;
+    if(direction == 'down') snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    };
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
