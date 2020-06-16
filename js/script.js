c715455cc9f5268019efece09328ac91a8b31000
PauloAlves8039 <pj38alves@gmail.com>
2020-06-15 21:10:58 -0300

    Finalizando as ações do personagem

--- js/script.js
+++ js/script.js
@@ -81,6 +81,13 @@ function iniciarJogo() {
     if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
     if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -93,7 +100,12 @@ function iniciarJogo() {
     if(direction == 'up') snakeY -= box;
     if(direction == 'down') snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
